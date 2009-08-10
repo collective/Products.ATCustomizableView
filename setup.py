22df3a765b2eed39955f24b3922b3a68bd594335
keul <keul@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-10 12:55:21 +0000

    Fixed info stuff in setup.py
    git-svn-id: https://svn.plone.org/svn/collective/Products.ATCustomizableView/trunk@94320 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ version = '0.2.0'
 
 setup(name='Products.ATCustomizableView',
       version=version,
-      description="A Plone monkey-patch for change the standard Plone behaviour with default view",
+      description="A Plone product that change the standard Plone behaviour of the views menu",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -19,7 +19,7 @@ setup(name='Products.ATCustomizableView',
       keywords='plone atct default-view layout',
       author='Keul (Luca Fabbri)',
       author_email='luca.fabbri@redturtle.net',
-      url='http://www.plone.it/scopri/documentazione/how-to/controllo-at-default-view-in-plone',
+      url='http://svn.plone.org/svn/collective/Products.ATCustomizableView',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
