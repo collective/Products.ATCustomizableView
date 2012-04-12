e6afda1ca01ae0414c64e026b4c17e120e490450
keul <keul@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-04-12 12:49:57 +0000

    Minor egg fixex, I will migrate now to github
    git-svn-id: https://svn.plone.org/svn/collective/Products.ATCustomizableView/trunk@249348 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ version = '0.4.1'
 setup(name='Products.ATCustomizableView',
       version=version,
       description='A Plone product that change the standard Plone behaviour of the "Display" menu',
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
